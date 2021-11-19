c11dc14a66f3beb94de68840e1d19ea14fd7c098
Kirill <10lkirill@mail.ru>
2021-11-20 03:09:34 +0500

    Дописал алгортимн нахождения PI, с последующим выводом

--- handler.php
+++ handler.php
@@ -1,9 +1,22 @@
 <?php
 $precision = $_GET['precision'];
 $int_precision = intval($precision);
-echo $int_precision;
-for ($i=1, $<$int_precision, $i++)
+//echo $int_precision;
+    $theValue = 100000;
+    $bottom = 1;
+    $pi = 0;
+for ($i=1; $i<$theValue; $i++)
 {
-    
+    if($i%2==1)
+    {
+        $pi+=4/$bottom;
+    }
+    else
+    {
+        $pi-=4/$bottom;
+    }
+      $bottom+=2;
 }
+//echo $pi;
+echo number_format($pi, $precision);
 ?>
