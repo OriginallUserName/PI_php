71febd5261e3efe657f727319a5baafc61fd100c
Kirill <10lkirill@mail.ru>
2021-11-20 03:36:56 +0500

    Попытка исправить неточность вычислений

--- handler.php
+++ handler.php
@@ -4,7 +4,7 @@ $int_precision = intval($precision);
 //echo $int_precision;
     $theValue = 100000;
     $bottom = 1;
-    $pi = 0;
+    $pi = floatval(0);
 for ($i=1; $i<$theValue; $i++)
 {
     if($i%2==1)
@@ -16,6 +16,7 @@ for ($i=1; $i<$theValue; $i++)
         $pi-=4/$bottom;
     }
       $bottom+=2;
+    // $pi=round($pi,$i, PHP_ROUND_HALF_DOWN);
 }
 //echo $pi;
 echo number_format($pi, $precision);
