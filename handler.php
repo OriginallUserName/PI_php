2b8bbdda30b6e2712e3a909705166b7eb25ba2fa
Kirill <10lkirill@mail.ru>
2021-11-20 02:34:52 +0500

    Initial commit

---
+++ handler.php
@@ -0,0 +1,9 @@
+<?php
+$precision = $_GET['precision'];
+$int_precision = intval($precision);
+echo $int_precision;
+for ($i=1, $<$int_precision, $i++)
+{
+    
+}
+?>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP</title>
+</head>
+<body>
+    <form action="handler.php">
+     <div><label> Precision :<input type="number" name="precision"></label></div>
+        <div><input type="submit" value="go"></div>
+    </form>
+</body>
+</html>
+
+
+
